331b3dbbb5f632cc6910a4e064e12214a1001c09
asuishi <1053562072@qq.com>
2018-08-10 12:08:24 +0800

    添加测试用例

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 ### 一个简单的ESLint
 
 ### 安装
+```
     npm i myeslint -g
+```
 
 ## 使用
-
+```
     myeslint test.js [test2.js]
+```
+
+## 测试
+```
+    npm run test
+```
---
+++ test/no-with.test.js
@@ -0,0 +1,39 @@
+/**
+ * Tests for no-with rule.
+ */
+
+//------------------------------------------------------------------------------
+// Constants
+//------------------------------------------------------------------------------
+
+const RULE_ID = "no-with"
+const myeslint = require('../lib/myeslint')
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+test('test "no-with" with error', () => {
+    const code = "with(foo) { bar() }"
+    const config = { rules: {} };
+    config.rules[RULE_ID] = 1;
+
+    myeslint.verify(code, config);
+    const messages = myeslint.messages
+
+    expect(messages.length).toBe(1)
+    expect(messages[0].ruleId).toBe(RULE_ID)
+    expect(messages[0].message).toBe("Unexpected use of 'with' statement.")
+    expect(messages[0].node.type).toBe("WithStatement")
+})
+
+test('test "no-with" no error', () => {
+    const code = "var a= 5"
+    const config = { rules: {} };
+    config.rules[RULE_ID] = 1;
+
+    myeslint.verify(code, config);
+    const messages = myeslint.messages
+
+    expect(messages.length).toBe(0)
+})
